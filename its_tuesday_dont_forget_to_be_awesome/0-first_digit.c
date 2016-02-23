5325bccb4646aed2d89db8002dcd04e89a388fe6
bilalbarki <bilalbarki@gmail.com>
2016-02-23 16:01:25 +0000

    added a comment

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,7 +1,7 @@
 int first_digit(int n){
   int temp;
   for (temp=0;n!=0;n=n/10)
-    temp=n;
+    temp=n; /*loop will end with first digit in temp*/
   if (temp<0)
     temp=temp*(-1);
   return temp;
