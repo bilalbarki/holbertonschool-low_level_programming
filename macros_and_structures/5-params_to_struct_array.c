338d89ffa7ac0182d3d26a425606ef8a1af8a8c6
bilalbarki <bilalbarki@gmail.com>
2016-05-07 07:17:26 +0000

    Separated string_split.c

--- macros_and_structures/5-params_to_struct_array.c
+++ macros_and_structures/5-params_to_struct_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "param_struct.h"
+
 char **string_split(char *string);
-int word_count(char *);
 int length(char *);
 void copy_string(struct Param *str_object, char *str);
 
@@ -28,47 +28,6 @@ struct Param *params_to_struct_array(int ac, char **av){
     return param;
 }
 
-/*splits each word of a string and stores 
-  them in a array of strings*/
-char **string_split(char *string){
-  int i,j,w_count,temp;
-  char **split;
-  w_count=word_count(string);
-  if (w_count==0) return NULL;
-  split=malloc(sizeof(char *)*(w_count+1));
-  if (split==NULL) return NULL;
-  split[w_count]=NULL;
-  for (i=0,w_count=0;string[i]!='\0';i++,w_count++){
-    for(;string[i]==' ';i++);
-    if (string[i]=='\0') break;
-    temp=i;
-    for(j=0;string[temp]!='\0' && string[temp]!=' ';temp++,j++);
-    split[w_count]=malloc(sizeof(char)*(j+1));
-    if(split[w_count] == NULL){
-      for(w_count--; w_count>= 0; w_count--) free(split[w_count]);
-      free(split);
-      return NULL;
-    }
-    split[w_count][j]='\0';
-    for(j=0;string[i]!='\0' && string[i]!=' ';i++,j++)
-      split[w_count][j]=string[i];
-    if (string[i]=='\0') break;
-  }
-  return split;
-}
-
-/*counts words*/
-int word_count(char *string){
-  int i, w_count;
-  for (w_count=0,i=0;string[i]!='\0';i++){
-    for(;string[i]==' ';i++);
-    if (string[i]=='\0') break;
-    for(;string[i]!='\0' && string[i]!=' ';i++);
-    w_count++;
-    if (string[i]=='\0') break;
-  }
-  return w_count;
-}
 
 /*counts length*/
 int length(char *str){
