e2c47c289f99dad3fbb3762c52ab546b6255af95
bilalbarki <bilalbarki@gmail.com>
2016-07-09 21:42:51 +0000

    first look

--- hash_tables/ht_get.c
+++ hash_tables/ht_get.c
@@ -3,6 +3,7 @@
 
 List *find_key(List *list, const char *key);
 int ht_put(HashTable *hashtable, const char *key, const char *value);
+int str_compare(const char *s1, const char *s2);
 
 /*gets the paired value of a key from the given hashtable*/
 char *ht_get(HashTable *hashtable, const char *key) {
@@ -19,8 +20,18 @@ char *ht_get(HashTable *hashtable, const char *key) {
 /*searches for the key in the given list*/
 List *find_key(List *list, const char *key) {
   for ( ; list != NULL ; list = list ->next ) {
-    if ( strings_compare(key, list->key) == 0 )
+    if ( str_compare(key, list->key) == 0 )
       return list;
   }
   return NULL;
+}
+
+/*string compare function*/
+int str_compare(const char *s1, const char *s2){
+  int i;
+  for (i=0;*(s1+i)==*(s2+i);i++){
+    if (*(s1+i)=='\0')
+      return 0;
+  }
+  return (*(s1+i)-*(s2+i));
 }
