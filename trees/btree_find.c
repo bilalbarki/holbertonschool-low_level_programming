23c8dcabb96daf4908ee77e014b6190ab1a6a0bd
bilalbarki <bilalbarki@gmail.com>
2016-07-25 11:58:56 +0500

    first look

--- trees/btree_find.c
+++ trees/btree_find.c
@@ -8,20 +8,20 @@
  */
 BTree *btree_find(BTree *leaf, char *str)
 {
-  if( leaf != NULL )
-  {
-      if(strcmp(leaf->str, str) == 0)
-      {
-          return leaf;
-      }
-      else if(strcmp(str, leaf->str) > 0)
-      {
-	return btree_find(leaf->left, str);
-      }
-      else
-      {
-	return btree_find(leaf->right, str);
-      }
-  }
-  else return NULL;
+	if( leaf != NULL )
+	{
+		if(strcmp(leaf->str, str) == 0)
+		{
+			return leaf;
+		}
+		else if(strcmp(str, leaf->str) > 0)
+		{
+			return btree_find(leaf->left, str);
+		}
+		else
+		{
+			return btree_find(leaf->right, str);
+		}
+	}
+	else return NULL;
 }
