44496901796a3a792a3bb3cc636c9fb686786a00
bilalbarki <bilalbarki@gmail.com>
2016-08-15 07:21:22 +0000

    replaced snprintf

--- n_trees/read_folder.c
+++ n_trees/read_folder.c
@@ -1,18 +1,15 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <string.h>
-#include <errno.h>
 #include <dirent.h>
-#include <limits.h>
 #include "tree.h"
-int string_length(char *str);
 void filter_it(char *str);
 void filter_back(char *str);
 int ntree_insert(NTree **, char **, char *);
 char **string_split(const char *, char);
 void free_str_array(char **);
 void list_dir (char *, int, NTree **);
+char *string_concat(char *s1, char joiner, char *s2);
 
 /**
  * read_folder - Reads a folder and subfolders
@@ -22,8 +19,10 @@ void list_dir (char *, int, NTree **);
 NTree *read_folder(const char *path)
 {
         NTree *tree;
+	int len;
+        for (len=0; path[len]; len++);
         ntree_insert(&tree, NULL, (char *)path);
-        list_dir ((char *)path, string_length((char *)path), &tree);
+        list_dir ((char *)path, len, &tree);
         return tree;
 }
 
@@ -61,27 +60,14 @@ void list_dir (char * dir_name, int len, NTree **tree)
 		{
 			if (strcmp(d_name, "..")!=0 && strcmp(d_name, ".")!=0)
 			{
-				int path_length;
-				char path[PATH_MAX];
-				path_length = snprintf (path, 
-					 PATH_MAX,"%s/%s", dir_name, d_name);
+				char *path;
+				path = string_concat(dir_name, '/', d_name);
 				list_dir (path, len, tree);
+				free(path);
 			}
 		}
 	}
 	closedir (d);
-}
-
-/**
- * string_length - calculates string length
- * @str: input string
- * Description: Returns string length
- */
-int string_length(char *str)
-{
-        int i;
-        for (i=0;str[i]; i++);
-        return i;
 }
 
 /**
@@ -112,4 +98,28 @@ void filter_back(char *str) {
                         str[i] = '/';
                 }
         }
+}
+
+/**
+ * string_concat - Concats two string with a joiner
+ * @s1: First input string
+ * @joiner: The joiner is the middle char in between
+ * @s2: Second input string
+ * Description: Puts all files/folders to an ntree
+ */
+char *string_concat(char *s1, char joiner, char *s2){
+	int i,j,p;
+	char *concat;
+	for ( i=0; s1[i]!='\0'; i++ );
+	for ( j=0; s2[j]!='\0'; j++ );
+	j=j+i;
+	concat=malloc( sizeof(char) * (j+2) );
+	if (concat == NULL)
+		return NULL;
+	for ( p=0; s1[p]; p++ ) concat[p]=s1[p];
+	concat[p]=joiner;
+	p++;
+	for (i=0 ; s2[i]; p++, i++ ) concat[p]=s2[i];
+	concat[p]='\0';
+	return concat;
 }
