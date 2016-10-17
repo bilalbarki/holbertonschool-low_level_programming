d8510877d5bf9ac5d44fd2ec2144a17a15e8464d
bilalbarki <bilalbarki@gmail.com>
2016-10-18 01:02:22 +0500

    old version without floor casting

---
+++ maze/player.h
@@ -0,0 +1,13 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+typedef struct plyr {
+   float x;
+   float y;
+   int viewing_angle;
+   int height;
+   int FOV;
+   int distanceFromProjectionPlane;
+   float AngleBetweenSubsequentRays;
+} maze_player;
+#endif // PLAYER_H_INCLUDED
